201d2a53a115d13db719cb4695ebb992a66b6f52
Gildas-Landry <tsafackgil@gmail.com>
2023-11-21 22:58:46 +0100

    21/11/2023 update with objectif to deploy on vercel

--- app/api/index.php
+++ app/api/index.php
@@ -1,4 +1,11 @@
 <?php
 
-require __DIR__ . './../public/index.php';
+require __DIR__ . '../vendor/autoload.php';
+$app= require_once __DIR__ . '../bootstrap/app.php';
 
+$kernel =$app->make(Illuminatte\Contracts\Htp\kernel::class);
+$response= $kernel->handle(
+    $request=Illuminate\Http\Request::capture()
+);
+$reponse->send();
+$kernel->terminate($request, $response);
